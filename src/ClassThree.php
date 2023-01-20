10ea53f48f472ed0492499268ee3e3b2b862ae1f
Morteza Matbou <mortezamatbou91@gmail.com>
2023-01-20 19:42:47 +0330

    init files

---
+++ src/ClassThree.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace SecondPackage\Test;
+
+
+class ClassThree
+{
+
+    function __construct()
+    {
+    }
+
+    function __toString()
+    {
+        return "From second-package ClassThree";
+    }
+
+}
